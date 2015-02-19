523c8de9be474e5c2bb0b12cd02f3fb6420ba273
James <James@192.168.1.118>
2015-02-19 08:32:46 -0500

    #2 mantenimiento de pantalla PHP de Descuento Ingreso

---
+++ bin/form_descuento_ingresoList.php
@@ -0,0 +1,51 @@
+<?php
+session_start();
+//require_once ('lib/nusoap.php'); 
+require_once ('../../WSCaisa/lib/nusoap.php'); 
+//$wsdl= "http://". $_SERVER['SERVER_NAME']."/WSCaisa/MyService.php?wsdl";
+$wsdl= "http://". $_SERVER['SERVER_NAME']."/demos/WSCaisa/MyService.php?wsdl";
+
+
+//Create object that referer a web services 
+$client = new nusoap_client($wsdl,true); 
+
+//Give it value at parameter 
+	 $param = array(); 
+	 $result = $client->call('GetAllDescuento_Ingreso',$param,'','','',true);
+
+// fault if any
+if ($client->fault) {
+    echo 'Fault';
+    print_r($result);
+    echo '';
+} 
+else 
+{
+// Check for errors
+    $err = $client->getError();
+    if ($err) {
+        // Display the error
+        echo 'Error:'.$err;
+    } 
+	else 
+	{
+        // Display the result
+        if($result!=false)
+         {
+		    header('Content-type: application/json');
+			if($result[0]['id_usuario']==null)
+			{
+				$json = array("status" => 0, "info" => $result);
+				
+			}
+			else
+			{
+				$json = array("status" => 1, "info" => $result);
+				
+				
+			}
+			echo json_encode($json);
+         }
+    }
+}		
+?>
